69c15c2a57725cb0866448878e0f542efa853ee4
liberosuprani <lsuprani30@gmail.com>
2024-04-20 21:18:33 +0100

    hover e active do hamburgerButton, mudança de classes para ids

--- js/codigos.js
+++ js/codigos.js
@@ -1,9 +1,12 @@
 function showMobileNavbar() {
     let x = document.getElementById("mobileNavbar");
+    let y = document.getElementById("hamburgerButton");
 
     if (x.style.display === "flex") {
         x.style.display = "none";
+        y.src = "./img/hamburger-svgrepo-com.svg";
     } else {
         x.style.display = "flex";
+        y.src = "./img/hamburger-svgrepo-com-active.svg";
     }
 }
